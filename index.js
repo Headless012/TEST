055dcac585eded41eac5c2f7c363f3ac1611d59b
Headless012 <Andreapaolucci16@gmail.com>
2026-06-23 22:03:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ client.on('messageCreate', async message => {
 
 async function automateFreebeat(prompt, durationSeconds, originalMessage, imagePath = null, isVertical = false) {
   const browser = await chromium.launch({ 
-    headless: false,
+    headless: true,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
